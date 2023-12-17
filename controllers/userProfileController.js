d73c0b9f586af84acb277548634a1a4a42565436
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-12-17 10:55:11 +0530

    Update userProfileController.js

--- controllers/userProfileController.js
+++ controllers/userProfileController.js
@@ -5,9 +5,19 @@ const UserProfile = require('../models/userProfileModel');
 exports.createProfile = async (req, res) => {
   try {
     // TODO: Extract the user ID from the request parameters
+    const userId = req.params.userId;
+
     // TODO: Check if a profile already exists for the user, send a 400 response if exists
+    const existingProfile = await userProfileModel.findOne({userId});
+    if(existingProfile) {
+      return res.status(400).json({ message: 'Profile already exists for this user'});
+    }
     // TODO: Create a new user profile and send a success response
-    // res.status(201).json({ message: 'User profile created successfully', userProfile });
+    const userProfile = new userProfileModel({userId});
+    await userProfile.save();
+
+    res.status(201).json({ message: 'User profile created successfully', userProfile });
+    
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
@@ -17,9 +27,18 @@ exports.createProfile = async (req, res) => {
 exports.createList = async (req, res) => {
   try {
     // TODO: Extract the user ID from the request parameters and list name from the request body
+    const userId = req.params.userId;
+    const listName = req.body.listName;
     // TODO: Find the user profile, send a 404 response if not found
+    const userProfile = await userProfileModel.findOne({userId});
+    if(!userProfile){
+      return res.status(404).json({ message: 'User profile not found'});
+    }
     // TODO: Create a new list, add it to the user profile, and send a success response
-    // res.status(201).json({ message: 'List created successfully', list: newList });
+    userProfile.lists.push({ listName, items: []});
+    await userProfile.save();
+
+    res.status(201).json({ message: 'List created successfully', list: newList });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
@@ -29,10 +48,24 @@ exports.createList = async (req, res) => {
 exports.addItemToList = async (req, res) => {
   try {
     // TODO: Extract the user ID and list ID from the request parameters, item name from the request body
+    const userId = req.params.userId;
+    const listId = req.params.listId;
+    const itemName = req.body.itemName;
     // TODO: Find the user profile, send a 404 response if not found
+    const userProfile = await userProfileModel.findOne({userId});
+    if(!userProfile){
+      return res.status(404).json({ message: 'User profile not found'});
+    }
     // TODO: Find the target list, send a 404 response if not found
+    const targetList = userProfile.lists.id(listId);
+    if(!targetList){
+      return res.status(404).json({ message: 'User profile not found'});
+    }
     // TODO: Add the item to the list, save the user profile, and send a success response
-    // res.json({ message: 'Item added to the list successfully', list: targetList });
+    targetList.items.push(listName);
+    await userProfile.save();
+    
+    res.json({ message: 'Item added to the list successfully', list: targetList });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
