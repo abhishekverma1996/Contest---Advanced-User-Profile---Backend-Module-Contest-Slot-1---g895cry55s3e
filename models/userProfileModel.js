0fbcf07971c10d036a114743418277ff445c4b62
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-12-17 10:54:38 +0530

    Update userProfileModel.js

--- models/userProfileModel.js
+++ models/userProfileModel.js
@@ -5,31 +5,31 @@ const mongoose = require('mongoose');
 // Define the schema for a list
 const listSchema = new mongoose.Schema({
   // TODO: Define the fields for the list schema
-  // listId: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   default: mongoose.Types.ObjectId,
-  // },
-  // name: {
-  //   type: String,
-  //   required: true,
-  // },
-  // items: [
-  //   {
-  //     type: String,
-  //   },
-  // ],
+  listId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  items: [
+    {
+      type: String,
+    },
+  ],
 });
 
 // Define the schema for a user profile
 const userProfileSchema = new mongoose.Schema({
   // TODO: Define the fields for the user profile schema
-  // user: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'User',
-  //   required: true,
-  //   unique: true,
-  // },
-  // lists: [listSchema],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true,
+  },
+  lists: [listSchema],
 });
 
 const UserProfile = mongoose.model('UserProfile', userProfileSchema);
